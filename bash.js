fe3e08fd911b49d5f1f1a9b4e4eb5f0cde49b94a
Miyuki Abe <miyuki0809@gmail.com>
2019-01-15 22:39:02 -0500

    updated

--- bash.js
+++ bash.js
@@ -1,16 +1,10 @@
 
+
 process.stdout.write("prompt > ");
 
 process.stdin.on("data", data => {
   const cmd = data.toString().trim();
 
-  if (cmd === "pwd") {
-    let directoryInfo = (process.cwd());
-    process.stdout.write(`Current Directory: ${directoryInfo}`);
-    process.stdout.write('promot > ')
-  } else {
     process.stdout.write("You typed: " + cmd);
     process.stdout.write("\nprompt > ");
-  }
-
 });
--- pwd.js
+++ pwd.js
@@ -1,9 +1,7 @@
-
-
-  if (cmd === "pwd") {
+module.exports = function (cmd){
+  if (cmd === 'pwd') {
     let directoryInfo = (process.cwd());
     process.stdout.write(`Current Directory: ${directoryInfo}`);
     process.stdout.write('promot > ')
-  } 
-
-});
+  }
+}
